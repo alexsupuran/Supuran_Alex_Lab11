8f9e7b7a6a36d72a5f9fcf18abb06cd902ea66ca
ALEX-CRISTIAN SUPURAN <alex.supuran@stud.ubbcluj.ro>
2023-12-24 17:36:31 +0200

    Add project files.

---
+++ Supuran_Alex_Lab11/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Supuran_Alex_Lab11.Data;
+namespace Supuran_Alex_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Supuran_Alex_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Supuran_Alex_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
